a0e0e704e0b8f063451f79fc322d4b811eb38d2f
Dane Sisinni <dfsisinni@gmail.com>
2015-11-28 01:36:36 -0500

    Login Authentication

--- src/com/example/hackwestern/HackwesternUI.java
+++ src/com/example/hackwestern/HackwesternUI.java
@@ -2,19 +2,20 @@ package com.example.hackwestern;
 
 import javax.servlet.annotation.WebServlet;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
+
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
 
 @SuppressWarnings("serial")
 @Theme("hackwestern")
 public class HackwesternUI extends UI {
+	
+	Subject currentUser;
 
 	@WebServlet(value = "/*", asyncSupported = true)
 	@VaadinServletConfiguration(productionMode = false, ui = HackwesternUI.class)
@@ -23,17 +24,14 @@ public class HackwesternUI extends UI {
 
 	@Override
 	protected void init(VaadinRequest request) {
-		final VerticalLayout layout = new VerticalLayout();
-		layout.setMargin(true);
-		setContent(new LoginForm());
+		currentUser = SecurityUtils.getSubject();
+		
+		if (currentUser.isAuthenticated() || currentUser.isRemembered()) {
+			
+		} else {
+			setContent(new LoginFormDesign());
+		}
 
-		Button button = new Button("Clickretert Me");
-		button.addClickListener(new Button.ClickListener() {
-			public void buttonClick(ClickEvent event) {
-				layout.addComponent(new Label("Thank you for clicking"));
-			}
-		});
-		layout.addComponent(button);
 	}
 
 }
